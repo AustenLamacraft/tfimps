92f3c40ac2eaf48fe0d6de745e3ccc5e7bd20162
mencia <beinat.mencia@gmail.com>
2018-05-28 14:58:44 +0100

    1) I changed the convergence condition for R in right_eigenvector(). 2) I introduced r_prec which sets the condition of convergence the right eigenvector 3) I changed the definition of X1 for an equivalent simpler one. 4) I introduced two new methods to express the energy as a sum of an onsite and NN term (instead using the trick with identity that you use in the definition of X1). This gives the same result. But I am not using it, I sticked to the idenity trick in X1.

--- tfimps.py
+++ tfimps.py
@@ -232,7 +232,7 @@ class Tfimps:
 
 
 if __name__ == "__main__":
-    #################################
+    #################################s
     # TRANSVERSE FIELD ISING
     #################################
     # physical and bond dimensions of MPS
